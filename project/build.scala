1ebee8d3818e1220a371be15c84a2ce9bb549515
John P. McCrae <john@mccr.ae>
2016-06-09 17:02:55 +0100

    Make SLF4J shut up

--- project/build.scala
+++ project/build.scala
@@ -23,14 +23,18 @@ object YuzuBuild extends Build {
       resolvers += Classpaths.typesafeReleases,
       resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
       libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.1" % "runtime",
+        "org.scalatra" %% "scalatra" % ScalatraVersion
+          excludeAll(ExclusionRule(organization="org.slf4j")),
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion
+          excludeAll(ExclusionRule(organization="org.slf4j")),
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test"
+          excludeAll(ExclusionRule(organization="org.slf4j")),
+//        "ch.qos.logback" % "logback-classic" % "1.1.1" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.10.v20150310" % "container"
           excludeAll(ExclusionRule(organization="org.slf4j")),
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
-        "io.spray" %% "spray-json" % "1.3.2"
+        "io.spray" %% "spray-json" % "1.3.2",
+        "org.slf4j" % "slf4j-nop" % "1.7.13"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
